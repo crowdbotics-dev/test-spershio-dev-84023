18940bd6b22c739df47aaf7d9578f05607589a8c
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 05:32:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-spershio-dev-84023",
+  url: "https://test-spershio-dev-84023.botics.co",
+  api: "https://test-spershio-dev-84023.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
